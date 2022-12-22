4c083859b9aace7458cecd4390923a0cbc11794d
Jose Soto <jsoto7087@gmail.com>
2022-12-21 22:32:38 -0500

    Added server request

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,26 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((resp) => resp.json())
+      .then((data) => setData(data))
+  }, [])
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {data.map((item) => {
+          return (
+            <ListingCard 
+              onData={item} 
+              key={item.id}
+            />
+          )
+        })}
       </ul>
     </main>
   );
